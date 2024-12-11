13cf84cb405d1cb4b1222f8bb0f1b7eecd8e05ca
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-12-11 22:47:36 +0700

    11-12-2567

--- api/index.js
+++ api/index.js
@@ -10,9 +10,14 @@ app.use(express.json());
 // MySQL Database Connection
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 30000 
 };
 
 const pool = mysql.createPool(dbConfig);
